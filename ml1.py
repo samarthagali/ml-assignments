10e4ed075d9b1332b97a57b5a5354ef5c4303c64
Samarth  Agali Prasad <samarthprasad440@gmail.com>
2022-05-26 17:18:08 +0530

    Add files via upload

---
+++ ml1.py
@@ -0,0 +1,36 @@
+#1
+s=input()
+print(s[::-1])
+
+#2
+s=input().split()
+for i in s:
+    print(i[0],end="")
+
+#3
+s=input().split()
+st=""
+for i in s:
+    for j in i:
+        if j.isupper()==True:
+            st+=j
+print(st)
+
+#4
+s=input().split()
+print(s.reverse())
+
+#5
+s=input().split("#")
+print(s[2])
+
+#6
+#append : Adds its argument as a single element to the end of a list. The length of the list increases by on
+l1=[1,2]
+l1.append([3])
+print(l1)
+#extend: Iterates over its argument and adding each element to the list and extending the list. The length of the list increases by number of elements in it’s argument.
+l1=[1,2]
+l1.extend([3])
+print(l1)
+
